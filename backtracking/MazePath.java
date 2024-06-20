04abbd383444ce49b2430e06afdd377a44262314
Sahil Garg <sahilgarg2801@gmail.com>
2024-06-20 12:19:21 +0530

    Backtracking -Maze Problem

---
+++ backtracking/MazePath.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MazePath {
+    public static void main(String[] args) {
+        // System.out.println(pathReturn("",3, 3));
+        // path("",3, 3);
+        System.out.println(pathDiagonal("", 3, 3));
+
+    }
+
+    static int count(int r, int c) {
+        if (r == 1 || c == 1) {
+            return 1;
+        }
+        int left = count(r - 1, c);
+        int right = count(r, c - 1);
+        return left + right;
+    }
+
+    static void path(String p, int r, int c) {
+        if (r == 1 && c == 1) {
+            System.out.println(p);
+            return;
+        }
+        if (r > 1)
+            path(p + "D", r - 1, c);
+        if (c > 1)
+            path(p + "R", r, c - 1);
+    }
+
+    static ArrayList<String> pathReturn(String p, int r, int c) {
+        if (r == 1 && c == 1) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            // System.out.println(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        if (r > 1) {
+            list.addAll(pathReturn(p + "D", r - 1, c));
+        }
+        if (c > 1) {
+            list.addAll(pathReturn(p + "R", r, c - 1));
+        }
+        return list;
+    }
+
+    static ArrayList<String> pathDiagonal(String p, int r, int c) {
+        if (r == 1 && c == 1) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        if (r > 1 && c > 1) {
+            list.addAll(pathDiagonal(p + "D", r - 1, c - 1));
+        }
+        if (r > 1) {
+            list.addAll(pathDiagonal(p + "V", r - 1, c));
+        }
+        if (c > 1) {
+            list.addAll(pathDiagonal(p + "H", r, c - 1));
+        }
+        return list;
+    }
+}
